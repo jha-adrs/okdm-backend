2add7e1b13603511666d70d368de2a7b1cb14af9
Aadarsh Jha <aadarshjha6783@gmail.com>
2024-10-23 01:05:48 +0000

    Prettier Fix

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,14 @@ import helmet from 'helmet';
 import compression from 'compression';
 import cors from 'cors';
 import passport from 'passport';
-import expressSession from 'express-session'
+import expressSession from 'express-session';
 import httpStatus from 'http-status';
 import RedisStore from 'connect-redis';
-import { createClient } from 'redis'
+import { createClient } from 'redis';
 import { xss } from './middleware/xss';
-import { config } from './config/config'
-import "./config/auth" //Passport strategies
-import "./config/db" //Prisma client connection
+import { config } from './config/config';
+import './config/auth'; //Passport strategies
+import './config/db'; //Prisma client connection
 //Routes
 import authRoutes from './routes/v1/auth.routes';
 import profileRoutes from './routes/v1/profile.routes';
@@ -21,58 +21,62 @@ import morgan from './middleware/morgan';
 import errorHandler from './middleware/error-handler';
 import logger from './config/logger';
 import rateLimiter from './middleware/rate-limiter';
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(helmet())
-app.use(compression())
-app.use(cors({
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(compression());
+app.use(
+  cors({
     origin: config.allowedOrigins,
     credentials: true
-}))
+  })
+);
 // Allow preflight
 app.options('*', cors());
-app.use(xss())
+app.use(xss());
 
 const client = createClient({
-    url: config.redisUrl
+  url: config.redisUrl
 });
-client.connect()
-client.on("error", function (error) {
-    console.error(error);
+client.connect();
+client.on('error', function (error) {
+  console.error(error);
 });
 const redisStore = new RedisStore({
-    client,
-    prefix: "okdm:"
-})
+  client,
+  prefix: 'okdm:'
+});
 
-app.use(expressSession({
+app.use(
+  expressSession({
     secret: config.jwt.secret,
     resave: false,
     saveUninitialized: false,
     store: redisStore,
-    cookie:{
-        secure: config.env === "production",
+    cookie: {
+      secure: config.env === 'production'
     }
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(morgan.successHandler)
-app.use(morgan.errorHandler)
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(morgan.successHandler);
+app.use(morgan.errorHandler);
 
-app.use("/v1/auth", rateLimiter.authRateLimiterMiddleware, authRoutes);
+app.use('/v1/auth', rateLimiter.authRateLimiterMiddleware, authRoutes);
 
-app.use(rateLimiter.otherRouteRateLimiterMiddleware) //Rate limiter for all other routes
-app.use("/v1/profile", profileRoutes);
-app.use("/v1/links", linkRoutes);
+app.use(rateLimiter.otherRouteRateLimiterMiddleware); //Rate limiter for all other routes
+app.use('/v1/profile', profileRoutes);
+app.use('/v1/links', linkRoutes);
 
 app.use((req, res) => {
-    res.status(httpStatus.NOT_FOUND).send("Not Found")
+  res.status(httpStatus.NOT_FOUND).send('Not Found');
 });
 
 app.use(errorHandler);
 
 app.listen(config.port, () => {
-    logger.info(`Server running on port ${config.port}`)
-})
+  logger.info(`Server running on port ${config.port}`);
+});
